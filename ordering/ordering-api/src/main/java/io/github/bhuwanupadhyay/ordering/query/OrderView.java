bbc6901ec3d766f5aa7ac0f3efc8620aab47cded
Bhuwan Pd. Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-12-13 08:45:22 +0400

    jooq

--- ordering/ordering-api/src/main/java/io/github/bhuwanupadhyay/ordering/query/OrderView.java
+++ ordering/ordering-api/src/main/java/io/github/bhuwanupadhyay/ordering/query/OrderView.java
@@ -1,5 +1,11 @@
 package io.github.bhuwanupadhyay.ordering.query;
 
+import io.github.bhuwanupadhyay.ordering.query.gen.tables.records.OmsOrderRecord;
+
 class OrderView {
+
+    public OrderView(OmsOrderRecord record) {
+
+    }
 
 }
--- ordering/ordering-api/src/main/java/io/github/bhuwanupadhyay/ordering/query/QueryEndpoints.java
+++ ordering/ordering-api/src/main/java/io/github/bhuwanupadhyay/ordering/query/QueryEndpoints.java
@@ -2,16 +2,18 @@ package io.github.bhuwanupadhyay.ordering.query;
 
 
 import io.github.bhuwanupadhyay.ordering.EndpointResponse;
-import io.github.bhuwanupadhyay.ordering.query.gen.tables.OmsOrder;
 import io.github.bhuwanupadhyay.ordering.query.gen.tables.records.OmsOrderRecord;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
+import org.jooq.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
+
+import static io.github.bhuwanupadhyay.ordering.query.gen.tables.OmsOrder.OMS_ORDER;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping(QueryEndpoints.ORDERS)
@@ -23,9 +25,12 @@ public class QueryEndpoints {
 
     @GetMapping
     public EndpointResponse<List<OrderView>> listOrders() {
-        dsl.select(OmsOrderRecord.class)
-        return new EndpointResponse<>(Collections.emptyList());
+        Result<OmsOrderRecord> records = dsl.selectFrom(OMS_ORDER).fetch();
+        return new EndpointResponse<>(toViews(records));
     }
 
+    private List<OrderView> toViews(Result<OmsOrderRecord> records) {
+        return records.stream().map(OrderView::new).collect(toList());
+    }
 
 }
