6de327d00f2396d13baafe6bb50fd438b6c372a5
Bhuwan Pd. Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-12-14 06:02:37 +0400

    jooq

---
+++ ordering/ordering-api/src/test/java/io/github/bhuwanupadhyay/ordering/query/QueryEndpointsProviderTest.java
@@ -0,0 +1,10 @@
+package io.github.bhuwanupadhyay.ordering.query;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+class QueryEndpointsProviderTest {
+
+}
