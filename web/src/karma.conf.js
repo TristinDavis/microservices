400a05466c65903735d27cf759f08efe2510c4c5
Bhuwan Pd. Upadhyay <bhuwan.upadhyay49@gmail.com>
2019-01-03 04:11:40 +0400

    cloud components

--- web/src/karma.conf.js
+++ web/src/karma.conf.js
@@ -4,13 +4,21 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'pact'],
+    pact: [{
+      port: 1234,
+      consumer: "web",
+      provider: "api",
+      dir: "dist/pacts",
+      log: "dist/pacts-log"
+    }],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@pact-foundation/karma-pact')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
