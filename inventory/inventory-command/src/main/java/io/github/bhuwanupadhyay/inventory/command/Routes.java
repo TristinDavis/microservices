e3b36a46ff0977dd271a5b74df3e3d07eb6198ed
Bhuwan Pd. Upadhyay <bhuwan.upadhyay49@gmail.com>
2019-01-03 09:39:14 +0400

    added configs

---
+++ inventory/inventory-command/src/main/java/io/github/bhuwanupadhyay/inventory/command/Routes.java
@@ -0,0 +1,33 @@
+package io.github.bhuwanupadhyay.inventory.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+@RequiredArgsConstructor
+class Routes {
+
+    private final InventoryHandler handler;
+
+    @Bean
+    RouterFunction<ServerResponse> routes() {
+        return nest(
+                path("/inventories"),
+                nest(
+                        accept(APPLICATION_JSON),
+                        route(POST("/")
+                                .and(contentType(APPLICATION_JSON)), handler::createPerson)
+                )
+        );
+    }
+
+
+}
