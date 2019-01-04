cf9dec446a774cba69a0dac4eb48cd273f06be95
Bhuwan Pd. Upadhyay <bhuwan.upadhyay49@gmail.com>
2019-01-04 07:56:12 +0400

    inventory CQRS

--- gateway/src/main/java/io/github/bhuwanupadhyay/gateway/GatewayApp.java
+++ gateway/src/main/java/io/github/bhuwanupadhyay/gateway/GatewayApp.java
@@ -2,13 +2,7 @@ package io.github.bhuwanupadhyay.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
