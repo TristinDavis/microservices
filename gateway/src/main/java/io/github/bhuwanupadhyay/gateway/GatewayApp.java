e62347c7cc17f3a7e7f0ce0f2c8871d9296f01af
Bhuwan Pd. Upadhyay <bhuwan.upadhyay49@gmail.com>
2019-01-04 07:51:14 +0400

    inventory CQRS

--- gateway/src/main/java/io/github/bhuwanupadhyay/gateway/GatewayApp.java
+++ gateway/src/main/java/io/github/bhuwanupadhyay/gateway/GatewayApp.java
@@ -2,60 +2,28 @@ package io.github.bhuwanupadhyay.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableConfigurationProperties(DiscoveryLocatorProperties.class)
 public class GatewayApp {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
     }
 
-    @RequestMapping("/hystrix-fallback")
-    public String hystrixFallback() {
-        return "This is a fallback";
-    }
-
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        //@formatter:off
-        return builder.routes()
-                .route("inventory_route", r -> r.path("/inventory")
-                        .uri("http://httpbin.org"))
-                .build();
-        //@formatter:on
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient,
+                                                                             DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
     }
-
-//    @Bean
-//    RedisRateLimiter redisRateLimiter() {
-//        return new RedisRateLimiter(1, 2);
-//    }
-
-//    @Bean
-//    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
-//        return http.httpBasic().and()
-//                .csrf().disable()
-//                .authorizeExchange()
-//                .pathMatchers("/anything/**").authenticated()
-//                .anyExchange().permitAll()
-//                .and()
-//                .build();
-//    }
-//
-//    @Bean
-//    public MapReactiveUserDetailsService reactiveUserDetailsService() {
-//        UserDetails user = SecurityProperties.User.withDefaultPasswordEncoder()
-//                .username("user").password("password").roles("USER")
-//                .build();
-//        return new MapReactiveUserDetailsService(user);
-//    }
-
 
 }
